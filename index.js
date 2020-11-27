c5bfacdcbc8e7fb9b106e0e53d7462e91152ff92
Chris Cullen <chriscullen42@gmail.com>
2020-11-27 13:01:36 +0000

    Added simple csv output and removed comma from date format

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const datelib = require("./lib/datelib")
 const dateIterator = datelib.iterateMonths(new Date("2020-01-01"), 12)
 const result = [...dateIterator]
 
-console.log(JSON.stringify(result,null,"\t"))
+let csv = "payday,bonus\n"
+result.map(r=>csv+=`${r.payDay},${r.bonusDay}\n`)
+console.log(csv)
